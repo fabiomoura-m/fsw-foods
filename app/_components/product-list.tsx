c085c0ee88689624e982a1839024d43026a0518a
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-05-01 16:06:23 -0300

    feat: add product list and product item components

---
+++ app/_components/product-list.tsx
@@ -0,0 +1,30 @@
+import { db } from "../_lib/prisma";
+import ProductItem from "./product-item";
+
+const ProductList = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    take: 10,
+  });
+
+  return (
+    <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <ProductItem product={product} key={product.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
