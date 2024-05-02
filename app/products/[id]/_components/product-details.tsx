dcb82c3083681120d95dee420c82dc5f7a7d3483
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-05-02 16:28:03 -0300

    fix: show delivery time on product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -102,30 +102,26 @@ const ProductDetails = ({
 
         <Card className="mt-6 flex justify-around py-2">
           <div className="flex flex-col items-center">
-            <div className="flex items-center gap-2 text-muted-foreground">
+            <div className="flex items-center gap-[5px] text-muted-foreground">
               <span className="text-xs">Entrega</span>
               <BikeIcon size={14} />
             </div>
             {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-sm font-semibold">
+              <p className="text-xs font-semibold">
                 {formatCurrency(Number(product.restaurant.deliveryFee))}
               </p>
             ) : (
-              <p className="text-sm font-semibold">Grátis</p>
+              <p className="text-xs font-semibold">Grátis</p>
             )}
           </div>
           <div className="flex flex-col items-center">
-            <div className="flex items-center gap-2 text-muted-foreground">
+            <div className="flex items-center gap-1 text-muted-foreground">
               <span className="text-xs">Entrega</span>
               <TimerIcon size={14} />
             </div>
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-sm font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className="text-sm font-semibold">Grátis</p>
-            )}
+            <p className="text-xs font-semibold">
+              {product.restaurant.deliveryTimeMinutes} min
+            </p>
           </div>
         </Card>
 
