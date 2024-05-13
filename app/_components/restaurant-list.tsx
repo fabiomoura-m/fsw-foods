1fbbcb2c7afad4f93318df562e063ac44a34963d
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-05-13 15:08:05 -0300

    feat: ensure restaurant can be favorited

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -1,13 +1,26 @@
+import { getServerSession } from "next-auth";
 import { db } from "../_lib/prisma";
 import RestaurantItem from "./restaurant-item";
+import { authOptions } from "../_lib/auth";
 
 const RestaurantList = async () => {
+  const session = await getServerSession(authOptions);
   const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
 
   return (
     <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
-        <RestaurantItem restaurant={restaurant} key={restaurant.id} />
+        <RestaurantItem
+          restaurant={restaurant}
+          key={restaurant.id}
+          userId={session?.user.id}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
       ))}
     </div>
   );
