66ed18812ed4543dfc4fcd358f2cf604fc3eca2f
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-05-15 16:18:03 -0300

    style: search restaurant page desktop

--- app/restaurants/_components/restaurants.tsx
+++ app/restaurants/_components/restaurants.tsx
@@ -35,15 +35,17 @@ const Restaurants = ({ userFavoriteRestaurants }: RestaurantsProps) => {
 
   return (
     <>
-      <Header />
-      <div className="px-5 py-6">
+      <div className="lg:border-b">
+        <Header hasSearch={true} />
+      </div>
+      <div className="px-5 py-6 lg:container lg:py-10">
         <h2 className="mb-6 text-lg font-semibold">Restaurantes Encontrados</h2>
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 lg:grid lg:grid-cols-3 xl:grid-cols-4">
           {restaurants.map((restaurant) => (
             <RestaurantItem
               key={restaurant.id}
               restaurant={restaurant}
-              className="min-w-full max-w-full"
+              className="min-w-full max-w-full lg:min-w-full lg:max-w-full"
               userFavoriteRestaurants={userFavoriteRestaurants}
             />
           ))}
